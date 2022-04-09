36556566159191b52508c06d70e87f65fbd08b53
Himanshu Dixit <101091797+himanshudixit1701@users.noreply.github.com>
2022-04-09 22:50:04 +0530

    Add files via upload
    if statement program

---
+++ IF statement program.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+int main()
+{
+   int a;
+    printf("Enter a number\n");
+    scanf("%d",&a);
+    if(a>=0)
+    {
+        printf("HIMANSHU YOUR NUMBER IS POSITIVE\n");
+    }
+
+    if(a<0)
+    {
+    printf("HEY HIMANSHU YOUR ENTERED NUMBER IS NEGATIVE\n");
+    }
+    return 0;
+}
