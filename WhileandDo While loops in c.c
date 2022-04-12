447b5b3b9be288daeb02e54a63c139ece7dab92c
Himanshu Dixit <101091797+himanshudixit1701@users.noreply.github.com>
2022-04-12 23:24:08 +0530

    Add files via upload
    while/dowhile  loops in c

---
+++ WhileandDo While loops in c.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+int main()
+
+{
+    int i=1;
+    while(i<=10){
+        printf("%d\n",i);
+        i = i+1;
+    }
+    int a=0;
+
+    do{
+        printf("%d\n",a);
+        a=a+2;
+
+    }while(a<=20);
+
+    return 0;
+}
