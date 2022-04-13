f38d2c7b0b918d67f50f53a7248db5e2040aaebd
Himanshu Dixit <101091797+himanshudixit1701@users.noreply.github.com>
2022-04-13 23:41:15 +0530

    Update if else statement in c.c

--- if else statement in c.c
+++ if else statement in c.c
@@ -5,7 +5,7 @@ int main()
     int a;
     printf("Enter The Number\n");
     scanf("%d",&a);
-   // printf("%d",a);
+   
     if(a<=10){
         printf("your entered number is either 10 or lesser than 10%d \n",a);
 
